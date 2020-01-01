17d9e9d4947a3d4cadb2d67c646f3956ab208482
Jakebas10 <jake.bas10@mac.com>
2019-12-31 19:44:59 -0600

    added logic to signup form

---
+++ includes/dbconfig.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+    $host = '127.0.0.1';
+    $user = 'jbasten';
+    $pass = '';
+    $db = 'social_media_app';
+
+    $conn = mysqli_connect($host, $user, $pass, $db);
+
+    if(!$conn) {
+        die("Could not connect: " . ysqli_error($conn));
+    }
+
+?>
---
+++ includes/signup.inc.php
@@ -0,0 +1,41 @@
+<?php
+    if (isset($_POST['signup-submit'])) {
+
+        try {
+            require 'dbconfig.inc.php';
+        } catch(Exception $e) {
+            header("Location: ../signup.php?error=dbConnIssue");
+            exit();
+        }
+
+        $username = $_POST['username'];
+        $pwd = $_POST['pwd'];
+        $pwd_confirmed = $_POST['pwd-confirm'];
+
+        if (empty($username) || empty($pwd) || empty($pwd_confirmed)) {
+            header("Location: ../signup.php?error=emptyFields&username=" . $username);
+            exit();
+        } else if (!preg_match("/^[a-zA-Z0-9]*$/", $username)) {
+            header("Location: ../signup.php?error=malformedEmail&username=" . $username);
+            exit();
+        } else if ($pwd != $pwd_confirmed) {
+            header("Location: ../signup.php?error=malformedEmail&pwdConfirmedDoesntMatch&username=" . $username);
+            exit();
+        } else {
+            $username = mysqli_real_escape_string($conn, $username);
+            $pwd = password_hash($pwd, PASSWORD_DEFAULT);
+        }
+
+        $query = "insert into users(username, password) values ('$username', '$pwd')";
+
+        if (mysqli_query($conn, $query)) {
+            header("Location: ../index.php?action=accountCreated&username=" . $username);
+            exit();
+        } else if (mysqli_errno($conn) == 1062) {
+            header("Location: ../signup.php?error=duplicateUsername&username=" . $username);
+        } else {
+            echo mysqli_error($conn);
+        }
+
+    }
+?>
