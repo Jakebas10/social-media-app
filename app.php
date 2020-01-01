d336df0bb6f8dd9471698f16c28f9db6b90f4feb
Jakebas10 <jake.bas10@mac.com>
2020-01-01 16:38:48 -0600

    added post form

---
+++ app.php
@@ -0,0 +1,9 @@
+<div class="username-label">
+    <?php
+        echo "<h2>Hello, " . $_SESSION['username'] . "!</h2>";
+    ?>
+</div>
+<form class="post-form" action="includes/sendpost.inc.php" method="post">
+    <textarea class="form-control" rows="2" cols="50" name="post-content" placeholder="What do you want to post?"></textarea>
+    <input class="btn btn-primary btn-block" type="submit" name="submit-post" value="Post">
+</form>
--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <div class="center wrapper-main">
             <?php
                 if (isset($_SESSION['username'])) {
-                    echo "<h1>" . $_SESSION['username'] . " is logged in</h1>";
+                    require "app.php";
                 } else {
                     echo "<h1>you are logged out</h1>";
                 }
